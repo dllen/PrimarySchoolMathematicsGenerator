0b8ce4dd3405fda10e39f61e3651120e0add402d
shichaopeng <shichaopeng@kuaishou.com>
2026-07-19 12:13:58 +0800

    feat(constants): add options enums and knowledge points dictionary

---
+++ src/constants/options.js
@@ -0,0 +1,12 @@
+export const GRADES = ['1', '2', '3', '4', '5', '6'];
+export const SEMESTERS = ['上', '下'];
+export const QUESTION_TYPES = ['arithmetic', 'application', 'olympiad'];
+export const DIFFICULTIES = ['easy', 'medium', 'hard'];
+export const ANSWER_MODES = ['hidden', 'inline', 'separate'];
+export const PROBLEM_TYPES = ['result', 'operand'];
+
+export const DIFFICULTY_TO_LEVEL = {
+  easy: 1,
+  medium: 2,
+  hard: 3,
+};
